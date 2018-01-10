49c22542cd6bc8a7499a152b4b03e120040eacfe
Tony FERRARI <tferrari@e3r3p20.42.fr>
2018-01-10 19:27:19 +0100

    algo ok, reste mise en forme du code propre

--- rate.py
+++ rate.py
@@ -19,11 +19,10 @@ def new_grad(hold, m):
 			tmp = hold[i]
 	for j in range(m):
 		km.append(hold[j] / tmp)
-	print km
 	return km
 
 def estimate(theta0, theta1, X):
-	return theta0 + (theta1 * X)
+	return (theta0 + (theta1 * X))
 
 def derivate(m, km, price, theta0, theta1):
 	ret = 0.0
@@ -31,7 +30,7 @@ def derivate(m, km, price, theta0, theta1):
 	for i in range(m):
 		tmp = (estimate(theta0, theta1, km[i]) - price[i])
 		ret += tmp * tmp
-	ret /= (2 * m)
+	ret /= (2 * float(m))
 	return ret
 
 def fct_thetha0(m ,km, price, theta0, theta1):
@@ -47,11 +46,11 @@ def fct_thetha1(m ,km, price, theta0, theta1):
 	return ret
 
 def learnningRate(m):
-	return (0.00000001)
+	return (0.01)
 
 def graph(km, price, theta0, theta1):
 	plt.plot([km],[price], 'ro', markersize=4)
-	plt.plot([estimate(theta0, theta1, x) for x in range(250000)], "b", linewidth=3)
+	plt.plot([estimate(theta0, theta1, x) for x in range(2)], "b", linewidth=3)
 	plt.xlabel('Mileage')
 	plt.ylabel('Price')
 	plt.show()
@@ -75,16 +74,22 @@ del hold_km[0]
 del hold_price[0]
 m = len(hold_km)
 fl_m = float(m)
-theta0 = first_theta0(hold_km, hold_price, m)
+theta0 = 0.0
 theta1 = 0.0
 i = 0
 km = new_grad(hold_km, m)
 price = new_grad(hold_price, m)
-# while derivate(m, km, price, theta0, theta1) != 0 and i < 100000:
-# 	tmp_theta0 = learnningRate(fl_m) * (1 / fl_m) * (fct_thetha0(m - 1, km, price, theta0, theta1))
-# 	tmp_theta1 = learnningRate(fl_m) * (1 / fl_m) * (fct_thetha1(m - 1, km, price, theta0, theta1))
-# 	theta0 -= tmp_theta0
-# 	theta1 -= tmp_theta1
-# 	print(theta0, "    ", theta1, "    ")
-# 	i += 1
-# graph(hold_km, hold_price, theta0, theta1)
+while i < 10000:
+	tmp_theta0 = learnningRate(fl_m) * (1 / fl_m) * (fct_thetha0(m, km, price, theta0, theta1))
+	tmp_theta1 = learnningRate(fl_m) * (1 / fl_m) * (fct_thetha1(m, km, price, theta0, theta1))
+	theta0 = theta0 - tmp_theta0
+	theta1 = theta1 - tmp_theta1
+	# print("theta0 = {}, theta1 = {}".format(theta0, theta1))
+	i += 1
+print("theta0 = {}, theta1 = {}".format(theta0, theta1))
+theta1 *= 8290.0 / 240000.0
+theta0 *= 8290
+i = estimate(theta0 , theta1 , float(42000))
+print i
+print theta1
+# graph(km, price, theta0, theta1)
